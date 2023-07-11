a1e64d15b9454b501608b2bc84bc64ccbea14ff9
nurjohn tunteev <92734791+nurj0hn@users.noreply.github.com>
2023-07-11 20:37:06 +0600

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,14 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "geekstudio.tezsat@gmail.com"
-EMAIL_HOST_PASSWORD = "nhbiplfugfinfxtp"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
